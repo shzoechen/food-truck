6f30ea8ada34d5b51b714f8be804d019700b786b
Zoe Chen <shzoechen@gmail.com>
2016-05-26 08:33:34 -0700

    [fix] new .gitignore

--- client/apiKey.js
+++
@@ -1,3 +0,0 @@
-//AWS credentials
-var accessKeyId = 'AKIAJFXUVI5SSSYQF64Q';
-var secretAccessKey = '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';
--- server/config.js
+++
@@ -1,6 +0,0 @@
-// google map API credentials
-module.exports.googleAPIkey = "AIzaSyB0I0T8yjAKyFzR2oQW_UTy6pvqcsxty_Y";
-module.exports.tokenSecret = "mksgreenfieldproject";
-
-//mLab url
-module.exports.url = "mongodb://shzoechen:203252@ds017070.mlab.com:17070/foodtrucks";
